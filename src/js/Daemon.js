0935457a88d221215fb2e4c8bd2b0c0f16b2eaeb
Maria <mariaes2016@yandex.ru>
2021-08-10 16:58:19 +0300

    oop

--- src/js/Daemon.js
+++ src/js/Daemon.js
@@ -2,8 +2,7 @@ import Character from './Character';
 
 export default class Daemon extends Character {
   constructor(name) {
-    super(name);
-    this.type = 'Daemon';
+    super(name, 'Daemon');
     this.attack = 10;
     this.defense = 40;
   }
