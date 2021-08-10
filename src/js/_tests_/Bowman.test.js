043413a72a5c0bd51729035a136fcd10c46edfe9
Maria <mariaes2016@yandex.ru>
2021-08-10 15:49:39 +0300

    oop

--- src/js/_tests_/Bowman.test.js
+++ src/js/_tests_/Bowman.test.js
@@ -1,9 +1,48 @@
 import Bowman from '../Bowman';
 
 test('Получим объект Bowman', () => {
-  const description = new Bowman('bowman');
-  const result = {
-    name: 'bowman', type: 'Bowman', health: 100, level: 1, attack: 25, defense: 25,
+  const expected = {
+  	name: 'test',
+    type: 'Bowman'
+     health: 100, 
+     level: 1, 
+     attack: 25, 
+     defense: 25,
   };
-  expect(description).toEqual(result);
+  expect(new Bowman('test')).toEqual(expected);
+});
+
+test('Метод levelUp должен повышать level на 1, attack и defense на 20% и health = 100',  () => {
+	const test = new Bowman('test');
+	test.health = 50;
+	test.levelUp();
+
+  expect(test.level).toBe(2);
+  expect(test.attack).toBe(30);
+  expect(test.defence).toBe(30);
+  expect(test.health).toBe(100);
+});
+test('Метод levelUP должен выбросить ошибку если health = 0', () => {
+  const test = new Bowman('test');
+  test.health = 0;
+  expect(() => test.levelUP()).toThrowError('Нельзя повысить левел умершего');
+});
+
+test('if health = 0, output health = 0', () => {
+  const test = new Bowman('test');
+  test.health = 0;
+  test.damage(30);
+  expect(test.health).toBe(0);
+});
+
+test('if health > 0 , output health must be correct', () => {
+  const test = new Bowman('test');
+  test.damage(30);
+  expect(test.health).toBe(77.5);
+});
+
+test('if health became negative, output health must be 0', () => {
+  const test = new Bowman('test');
+  test.damage(200);
+  expect(test.health).toBe(0);
 });
