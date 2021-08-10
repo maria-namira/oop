a24ff445b045ceb242e28a503b3f850301790c5a
Maria <mariaes2016@yandex.ru>
2021-08-10 16:59:15 +0300

    oop

--- src/js/Magician.js
+++ src/js/Magician.js
@@ -2,8 +2,7 @@ import Character from './Character';
 
 export default class Magician extends Character {
   constructor(name) {
-    super(name);
-    this.type = 'Magician';
+    super(name,'Magician');
     this.attack = 10;
     this.defense = 40;
   }
